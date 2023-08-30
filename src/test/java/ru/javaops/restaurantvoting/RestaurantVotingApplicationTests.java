7e1f735f7193d36660c8f765176378c598ead650
Maksim Wojtk <wojtk.maksim@gmail.com>
2023-08-30 16:10:15 +0300

    Initialize repository

---
+++ src/test/java/ru/javaops/restaurantvoting/RestaurantVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javaops.restaurantvoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantVotingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
